82ddc78b2100e47ca5ebff7da180bf9de014452b
tanda8 <tanda88@gmail.com>
2014-09-16 11:09:49 -0500

    test2l

---
+++ export.sql
@@ -0,0 +1,45 @@
+--------------------------------------------------------
+--  File created - Tuesday-September-16-2014   
+--------------------------------------------------------
+REM INSERTING into CHARACTER_SETS
+SET DEFINE OFF;
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('big5','big5_chinese_ci','Big5 Traditional Chinese',2);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('dec8','dec8_swedish_ci','DEC West European',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('cp850','cp850_general_ci','DOS West European',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('hp8','hp8_english_ci','HP West European',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('koi8r','koi8r_general_ci','KOI8-R Relcom Russian',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('latin1','latin1_swedish_ci','cp1252 West European',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('latin2','latin2_general_ci','ISO 8859-2 Central European',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('swe7','swe7_swedish_ci','7bit Swedish',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('ascii','ascii_general_ci','US ASCII',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('ujis','ujis_japanese_ci','EUC-JP Japanese',3);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('sjis','sjis_japanese_ci','Shift-JIS Japanese',2);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('hebrew','hebrew_general_ci','ISO 8859-8 Hebrew',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('tis620','tis620_thai_ci','TIS620 Thai',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('euckr','euckr_korean_ci','EUC-KR Korean',2);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('koi8u','koi8u_general_ci','KOI8-U Ukrainian',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('gb2312','gb2312_chinese_ci','GB2312 Simplified Chinese',2);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('greek','greek_general_ci','ISO 8859-7 Greek',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('cp1250','cp1250_general_ci','Windows Central European',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('gbk','gbk_chinese_ci','GBK Simplified Chinese',2);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('latin5','latin5_turkish_ci','ISO 8859-9 Turkish',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('armscii8','armscii8_general_ci','ARMSCII-8 Armenian',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('utf8','utf8_general_ci','UTF-8 Unicode',3);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('ucs2','ucs2_general_ci','UCS-2 Unicode',2);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('cp866','cp866_general_ci','DOS Russian',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('keybcs2','keybcs2_general_ci','DOS Kamenicky Czech-Slovak',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('macce','macce_general_ci','Mac Central European',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('macroman','macroman_general_ci','Mac West European',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('cp852','cp852_general_ci','DOS Central European',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('latin7','latin7_general_ci','ISO 8859-13 Baltic',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('utf8mb4','utf8mb4_general_ci','UTF-8 Unicode',4);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('cp1251','cp1251_general_ci','Windows Cyrillic',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('utf16','utf16_general_ci','UTF-16 Unicode',4);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('utf16le','utf16le_general_ci','UTF-16LE Unicode',4);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('cp1256','cp1256_general_ci','Windows Arabic',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('cp1257','cp1257_general_ci','Windows Baltic',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('utf32','utf32_general_ci','UTF-32 Unicode',4);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('binary','binary','Binary pseudo charset',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('geostd8','geostd8_general_ci','GEOSTD8 Georgian',1);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('cp932','cp932_japanese_ci','SJIS for Windows Japanese',2);
+Insert into CHARACTER_SETS (CHARACTER_SET_NAME,DEFAULT_COLLATE_NAME,DESCRIPTION,MAXLEN) values ('eucjpms','eucjpms_japanese_ci','UJIS for Windows Japanese',3);
